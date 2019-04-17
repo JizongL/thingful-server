b968eefc2fc898c54bb58800156d489827b4e7d4
JizongL <2015rpro@gmail.com>
2019-04-17 11:48:32 -0400

    added bcryptjs and hashed passwords and changed test endpoints

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,10 +1,12 @@
 'use strict';
 const AuthService = require('../auth/auth-service');
+const bcrypt = require('bcryptjs')
 
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || '';
 
   let basicToken;
+  console.log(basicToken,'test basictoken')
   if (!authToken.toLowerCase().startsWith('basic')) {
     return res.status(401).json({ error: 'Missing basic token' });
   } else {
@@ -21,15 +23,20 @@ function requireAuth(req, res, next) {
     req.app.get('db'),
     tokenUserName
   )
-    .then(user => {
-      if (!user || user.password !== tokenPassword) {
-        return res.status(401).json({ error: 'Unauthorized request' });
-      }
+  .then(user => {
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized request' });
+    }
+    return bcrypt.compare(tokenPassword, user.password)
+      .then(passwordsMatch => {
+        if (!passwordsMatch) {
+          return res.status(401).json({ error: 'Unauthorized request'});
+        }
 
-  req.user = user;
-
-  next();
-})
+        req.user = user;
+        next();
+      });
+  })
   .catch(next);
 }
 
