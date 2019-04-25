149a8f888c999ab5d84a5233495c129a90aafbba
JizongL <2015rpro@gmail.com>
2019-04-24 20:05:14 -0400

    finished registration implimentation

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const usersRouter = express.Router()
 const jsonBodyParser = express.json()
+const path = require('path')
 const UsersService = require('./users-service')
+
 usersRouter
   .post('/',jsonBodyParser,(req,res,next)=>{
-    const {password,user_name} = req.body
+    const { password, user_name, full_name, nickname } = req.body
     for (const field of ['full_name', 'user_name', 'password'])
       if(!req.body[field])
         return res.status(400).json({
@@ -21,8 +23,29 @@ usersRouter
       .then(hasUserWithUserName=>{
         if(hasUserWithUserName)
           return res.status(400).json({ error: `Username already taken` })          
-          res.send('ok')
+      return UsersService.hashPassword(password)
+        .then(hashPassword=>{
+          const newUser = {
+            user_name,
+             password:hashPassword,
+             full_name,
+             nickname,
+             date_created: 'now()',
+          }
+        
+        
+      return UsersService.insertUser(
+         req.app.get('db'),
+         newUser
+       )
+         .then(user => {
+           res
+             .status(201)
+             .location(path.posix.join(req.originalUrl, `/${user.id}`))
+             .json(UsersService.serializeUser(user))
+         })
       })
+    })
       .catch(next)  
   })
   
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,6 +1,17 @@
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
-
+const xss = require('xss')
+const bcrypt = require('bcryptjs')
 const UsersService = {
+  insertUser(db,newUser){
+    return db
+      .insert(newUser)
+      .into('thingful_users')
+      .returning('*')
+      .then(([user])=>user)
+  },
+  hashPassword(password){
+    return bcrypt.hash(password,12)
+  },
   hasUserWithUserName(db, user_name) {
        return db('thingful_users')
          .where({ user_name })
@@ -23,6 +34,15 @@ const UsersService = {
 
       return null 
   },
+  serializeUser(user) {
+     return {
+       id: user.id,
+       full_name: xss(user.full_name),
+       user_name: xss(user.user_name),
+       nickname: xss(user.nick_name),
+       date_created: new Date(user.date_created),
+     }
+   },
 
 }
 
